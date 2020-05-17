a07c662293cbe3d92f7b12af2dac189b348d0f49
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-05-17 14:48:29 +0700

    Creat main method

--- src/CopyFileText.java
+++ src/CopyFileText.java
@@ -1,15 +1,29 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class CopyFileText {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the path of file you want to copy: ");
+        String originalFilePath = scanner.nextLine();
+
+
+        System.out.print("Please enter the path to create a copy: ");
+        String copyFilePath = scanner.nextLine();
+
+        copyFile(originalFilePath, copyFilePath);
+    }
+
+    public static void copyFile(String originalFilePath, String copyFilePath) {
         try {
-            File originalFile = new File("OriginalFile.txt");
+            File originalFile = new File(originalFilePath);
 
             if (!originalFile.exists()) {
                 throw new FileNotFoundException();
             }
 
-            File copyFile = new File("CopyFile.txt");
+            File copyFile = new File(copyFilePath);
             if (copyFile.createNewFile()) {
                 System.out.println("File created: " + copyFile.getName());
             } else {
